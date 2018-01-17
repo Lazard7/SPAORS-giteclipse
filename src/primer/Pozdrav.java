2c01b3783bba1bf085b3c3c8e2787026c3842f4c
Lazar-Grabovica <dmi.lazar.grabovica@student.pmf.uns.ac.rs>
2018-01-17 12:48:28 +0100

    aha

--- src/primer/Pozdrav.java
+++ src/primer/Pozdrav.java
@@ -3,13 +3,10 @@ package primer;
 public class Pozdrav {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 
 		System.out.println("Zdravo i tebi");
 		System.out.println("Zdravo");
-=======
 		System.out.println("Zdravo, Lazar Grabovica!");
->>>>>>> refs/heads/Lazar-Grabovica!
 	}
 
 }
